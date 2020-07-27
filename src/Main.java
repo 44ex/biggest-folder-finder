fba5f568c4a83d9ff82632648f0c25db78d1f9a1
44ex <44ex@list.ru>
2020-07-27 04:04:10 +0300

    dev

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,28 @@
 import java.io.File;
+import java.util.LinkedList;
+import java.util.List;
 import java.util.Set;
+import java.util.concurrent.ForkJoinPool;
 
 public class Main {
     public static void main(String[] args) {
-        String folderPath = "C:\\Users\\Dmitry\\IdeaProjects";
+        //String folderPath = "C:\\Users\\Dmitry\\IdeaProjects";
+        String folderPath = "C:\\Users\\Dmitry\\Desktop";
         File file = new File(folderPath);
+        long start = System.currentTimeMillis();
+
+        //System.out.println(getFolderSize(file));
+        FolderSizeCalculator calculator = new FolderSizeCalculator(file);
+        // класс, позволяющий запускать множество потоков
+        ForkJoinPool pool = new ForkJoinPool();
+        long size = pool.invoke(calculator);
+        System.out.println(size);
+
+        long duration = System.currentTimeMillis() - start;
+        System.out.println(duration + " ms");
+
 //        System.out.println(file.length());
-        System.out.println(getFolderSize(file));
+//        System.out.println(getFolderSize(file));
 
 //        Set keys = System.getProperties().keySet();
 //        for(Object key : keys){
@@ -26,4 +42,5 @@ public class Main {
         }
         return sum;
     }
+
 }
